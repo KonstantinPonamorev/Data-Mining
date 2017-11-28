451cb858337f7181de3ef3d4b046596752ac1b0d
deadwindxkn <32107179+deadwindxkn@users.noreply.github.com>
2017-11-28 22:28:06 +0300

    изменение в парсе адресов

--- AvitoRazdelenie.py
+++ AvitoRazdelenie.py
@@ -23,10 +23,6 @@ def get_total_pages(html):
 
 
 def write_csv(data):
-    #print(str(data['title']))
-    #print(data['price'])
-    #print(data['address'])
-    #print(data['myurl'])
     with open('kvartiry.csv', 'a', encoding = 'utf-8') as f:
         writer = csv.writer(f)
         writer.writerow( (data['number'],
@@ -36,14 +32,12 @@ def write_csv(data):
                           data['metro'],
                           data['address'],
                           data['myurl'] )  )
-        #sys.exit(0)
 
 
 
 def get_page_data(html):
     global number
     soup = BeautifulSoup(html, 'lxml')
-    #print(soup)
     ads = soup.find('div', class_='catalog-list', recursive = True).find_all('div', class_='item_table')
     for ad in ads:
         try:
@@ -55,13 +49,9 @@ def get_page_data(html):
         except:
             square = '-'
         try:
-            myurl = 'https://www.avito.ru' + ad.find('div', class_='description').find('h3', class_= 'title').find('a').get('href')+'>'
+            myurl = 'https://www.avito.ru' + ad.find('div', class_='description').find('h3', class_= 'title').find('a').get('href')
         except:
-<<<<<<< HEAD
-            myurl = '>'
-=======
             myurl = '-'
->>>>>>> upstream/SQL
         try:
             price = ad.find('div', class_='about').text.strip().split('р')[0].replace(' ','')
         except:
@@ -71,19 +61,20 @@ def get_page_data(html):
         except:
             metro = '-'
         try:
-            address = ad.find('p', class_='address').text.strip().split(',')[1] + ',' + ad.find('p', class_='address').text.strip().split(',')[2]
+            address = ad.find('p', class_='address').text.strip().split('м, ')[1]
         except:
             address = '-'
         if price != 'ценанеуказана':
             if price != 'Ценанеуказана':
-                data = { 'number':number,
-                         'rooms':rooms,
-                         'square':square,
-                         'price':price,
-                         'metro':metro,
-                         'address':address,
-                         'myurl':myurl}
-                write_csv(data)
+                if address != '-':
+                    data = { 'number':number,
+                             'rooms':rooms,
+                             'square':square,
+                             'price':price,
+                             'metro':metro,
+                             'address':address,
+                             'myurl':myurl}
+                    write_csv(data)
         number=number+1
 
 
@@ -91,7 +82,7 @@ def get_page_data(html):
 def main():
     with open('kvartiry.csv', 'a', encoding = 'utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(('Number','Rooms','Square','Price','Metro','Address','Url''>'))
+        writer.writerow(('Number','Rooms','Square','Price','Metro','Address','Url'))
     url = 'https://www.avito.ru/sankt-peterburg/kvartiry/prodam?p=1'
     base_url = 'https://www.avito.ru/sankt-peterburg/kvartiry/prodam?'
     page_part = 'p=' 
