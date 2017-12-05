4b7c5b86a605a7563b530ee821b2e74ff8fb4c6e
KonstantinPonamorev <32388911+KonstantinPonamorev@users.noreply.github.com>
2017-12-05 14:29:40 +0300

    SQL.py final + tables example
    удаление дублированных строк, итоговый бд файл

--- SQL.py
+++ SQL.py
@@ -49,3 +49,31 @@ except:
 c.commit()
 c.close()
 
+
+#input_file = pd.read_csv('kvartiry.csv', encoding='utf-8')
+#input_file = input_file.drop(['Number'], axis=1)
+	
+#input_file = input_file.drop(0, axis=0)
+#print(input_file)
+#sys.exit(0)
+#rdr = csv.DictReader(input_file,
+	#fieldnames = ['Number', 'Rooms','Square','Price','Metro','Address','Url'])
+
+c = db.connect(database='kvartiry.db')
+cu = c.cursor()
+with open('kvartirysql.csv','rt', encoding='utf-8') as input_file:
+	creader = csv.DictReader(input_file, delimiter=',')
+	to_db = [(i['Address'], i['Metro'], i['Rooms'], i['Square'], i['Price'], i['Url']) for i in creader]
+cu.executemany('''INSERT INTO kv
+	(kvaddress, kvmetro, kvrooms, kvsquare, kvprice, kvurl)
+	VALUES (?,?,?,?,?,?);''', to_db)
+#for rec in input_file:
+	#print(rec[1])
+	#print(rec[3])
+	#sys.exit(0)
+	#cu.execute('''INSERT INTO kv
+		#(kvrooms, kvsquare, kvprice, kvmetro, kvaddress, kvurl)
+		#VALUES (
+			#?, ?, ?, ?, ?, ?);''', (rec, rec, rec, rec, rec, rec))
+c.commit()
+c.close()
